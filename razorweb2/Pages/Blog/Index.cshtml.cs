0a2064dad3952661b23dfaca51c29f912f4739eb
PhanTien234 <minhtienp328@gmail.com>
2023-05-02 17:52:34 +0700

    done create paging for display item for page

--- razorweb2/Pages/Blog/Index.cshtml.cs
+++ razorweb2/Pages/Blog/Index.cshtml.cs
@@ -20,6 +20,12 @@ namespace razorweb2.Pages.Blog
 
         public IList<Article> Article { get;set; } = default!;
 
+        public const int ITEMS_PER_PAGE = 10;
+        [BindProperty(SupportsGet =true, Name = "p")]
+        public int currentPage { get; set; }
+        public int countPages{get; set; }
+
+
         public async Task OnGetAsync(string SearchString)
         {
             // if (_context.articles != null)
@@ -27,9 +33,21 @@ namespace razorweb2.Pages.Blog
             //     Article = await _context.articles.ToListAsync();
             // }
             // Sort Categories by DateTime
-            var qr = from a in _context.articles
+            
+            int totalArticle = await _context.articles.CountAsync();
+            countPages = (int)Math.Ceiling((double)totalArticle / ITEMS_PER_PAGE);
+
+            if(currentPage <1){
+                currentPage =1;
+            }else if(currentPage > countPages){
+                currentPage = countPages;
+            }
+
+            var qr = (from a in _context.articles
                     orderby a.Created descending
-                    select a;
+                    select a)
+                    .Skip((currentPage -1) *ITEMS_PER_PAGE)
+                    .Take(ITEMS_PER_PAGE);
 
            if(!string.IsNullOrEmpty(SearchString)){
             Article = qr.Where(a => a.Title.Contains(SearchString)).ToList();
