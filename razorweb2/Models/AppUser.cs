74190a0d9aecce916440781521d0f36dc228df56
PhanTien234 <minhtienp328@gmail.com>
2023-05-04 21:56:54 +0700

    add Home Address into Identity User

--- razorweb2/Models/AppUser.cs
+++ razorweb2/Models/AppUser.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace razorweb2.models
 {
     public class AppUser : IdentityUser
     {
-        
+        // Add field Home Address into Identity User
+        [Column(TypeName = "nvarchar")]
+        [StringLength(400)]
+        public string HomeAddress { get; set; }
     }
 }
